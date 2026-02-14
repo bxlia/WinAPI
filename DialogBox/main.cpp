08c4295b4ff2a4e6badf39a06935c3f319d7be14
Булат <4bxlia4@gmail.com>
2026-02-14 23:37:07 +0500

    DONE

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -1,31 +1,44 @@
-#include<Windows.h>
-#include"resource.h"
+#include <Windows.h>
+#include "resource.h"
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-INT WINAPI  WinMain(HINSTANCE hInsance, HINSTANCE hPrevInst, LPSTR IpCmdLine, INT nCmsShow)
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInsance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
-	return 0;
+    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+    return 0;
 }
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg) 
-	{
-	case WM_INITDIALOG:
-		break;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case IDOK:
-			MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION);
-				break;
-		case IDCANCEL:EndDialog(hwnd, 0);
-		}
-		break;
-	case WM_CLOSE:
-		EndDialog(hwnd, 0);
-	}
-	return FALSE;
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+    {
+        HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+        if (hIcon) {
+            SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+            SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+        }
+        return TRUE;
+    }
+
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+            MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION);
+            break;
+
+        case IDCANCEL:
+            EndDialog(hwnd, 0);
+            break;
+        }
+        return TRUE;
+
+    case WM_CLOSE:
+        EndDialog(hwnd, 0);
+        return TRUE;
+    }
+    return FALSE;
 }
