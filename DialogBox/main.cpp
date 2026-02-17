b35b9bda6e43a8e45712473731b6a1629889ce57
Булат <4bxlia4@gmail.com>
2026-02-17 17:25:46 +0500

    Copy in DialogBox DONE

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -18,14 +18,23 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
         if (hIcon) {
             SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
-            SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
         }
-        return TRUE;
     }
 
     case WM_COMMAND:
         switch (LOWORD(wParam))
         {
+        case IDC_BUTTON_COPY:
+        {
+            CONST INT SIZE = 256; //
+            CHAR sz_buffer[SIZE] = {};
+            HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+            HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+            SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer); //считываем текст из поля 'Login'
+            SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer); //загружаем текст из буфера в поле 'Password'
+            
+        }
+        break;
         case IDOK:
             MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION);
             break;
