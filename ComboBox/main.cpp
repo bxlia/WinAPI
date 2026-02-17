34cf91300584c6aabe36eb269db268ffaa4236eb
Булат <4bxlia4@gmail.com>
2026-02-17 18:52:46 +0500

    ComboBox start

---
+++ ComboBox/main.cpp
@@ -0,0 +1,36 @@
+#include<Windows.h>
+#include<iostream>
+#include"resource.h"
+
+CONST CHAR* ITEMS[] = { "This", "is", "my", "fitst", "Combo", "Box" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPARAM);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPARAM)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+		//AllocConsole();
+		for (int i = 0; i < sizeof(ITEMS) / sizeof(ITEMS[0]); i++)
+		{
+			// std::cout << ITEMS[i] << std::endl;
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)ITEMS[i]);
+		}
+	}
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
