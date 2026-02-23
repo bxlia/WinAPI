c9f4f88078d172683ae268fdeb925cef9bd24163
Булат <4bxlia4@gmail.com>
2026-02-23 12:06:42 +0500

    DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -31,12 +31,31 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPARAM)
 				// std::cout << ITEMS[i] << std::endl;
 				SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)ITEMS[i]);
 			}
+			SendMessage(hCombo, CB_SETCURSEL, 0, 0);
 		}
 		break;
-	case IDOK:
-		MessageBox(hwnd, "Вы выбрали пункт №... со значением .....", "Info", MB_OK | MB_ICONINFORMATION);
-		break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			CHAR sz_message[SIZE] = {};
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+		
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0); //Получаем номер выбранного текста
+			if (i != CB_ERR)
+			{
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer); //Считываем содержимое выбранной строки в строке буфер
+				sprintf_s(sz_message, SIZE, "Вы выбрали пункт №%d со значением '%s'.", i+1, sz_buffer);
+				MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			
+		}
+		break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+		}
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
--- ComboBox/resource.h
+++ ComboBox/resource.h
@@ -5,8 +5,7 @@
 #define IDD_DIALOG1                     101
 #define IDI_ICON1                       105
 #define IDC_COMBO                       1001
-#define IDC_BUTTON                      1002
-#define IDC_OK                          1002
+#define IDC_BUTTON2                     1004
 
 // Next default values for new objects
 // 
@@ -14,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
